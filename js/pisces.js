1bcf01253e9d0751589aa40c8b6892f16f093197
Taiju Muto <hello@tai2.net>
2014-05-25 21:53:11 +0900

    HTTP Server base implemented.

--- js/pisces.js
+++ js/pisces.js
@@ -15,9 +15,27 @@ var pisces = (function() {
     const MESSAGE_HEADER_SIZE = 1 + 3 + 16 + 4 + 8 + 20 + 4;
     const MESSAGE_BODY_LIMIT = UDP_PAYLOAD_SIZE - MESSAGE_HEADER_SIZE;
     const BUFFER_LIMIT = 64 * 1024;
+    const HTTP_LINE_LIMIT = 8192;
+    const CR = 0x0d;
+    const LF = 0x0a;
+    const PS_REQEST_LINE = 0x01;
+    const PS_HEADERS = 0x02;
+    const PS_BODY = 0x03;
+    const PS_END = 0x04;
+    const RESPONSE_TABLE = {
+        200 : "OK",
+        304 : "Not Modified",
+        400 : "Bad Request",
+        404 : "Not Found",
+        405 : "Method Not Allowed",
+        411 : "Length Required",
+        413 : "Entity Too Large",
+        500 : "Internal Server Error"
+    };
 
+    var udpSocketId, tcpSocketId;
     var messagesSent = [];
-    var socketId = null;
+    var sessionTable = {};
     var agent = {
         "start" : agent_start,
         "stop" : agent_stop,
@@ -53,7 +71,7 @@ var pisces = (function() {
     }
 
     chrome.sockets.udp.onReceive.addListener(function(info) {
-        if (socketId !== info.socketId) {
+        if (udpSocketId !== info.socketId) {
             return;
         }
 
@@ -83,6 +101,205 @@ var pisces = (function() {
         // TODO: error handling
     });
 
+    chrome.sockets.tcpServer.onAccept.addListener(function(info) {
+        if (tcpSocketId !== info.socketId) {
+            return;
+        }
+
+        chrome.sockets.tcp.setPaused(info.clientSocketId, false);
+    });
+
+    chrome.sockets.tcp.onReceive.addListener(function(info) {
+        var session;
+        var segment = new Uint8Array(info.data);
+
+        session = sessionTable[info.socketId];
+        if (!session) {
+            session = {
+                socketInfo : info,
+                method : null,
+                url : null,
+                version : null,
+                headers : {},
+                contentLength : 0,
+                responseCode : null,
+                state : PS_REQEST_LINE,
+                linePos : 0,
+                lineBuf : new Uint8Array(HTTP_LINE_LIMIT),
+                bodyRead : 0,
+            };
+            sessionTable[info.socketId] = session;
+        }
+
+        while (session.state != PS_END) {
+            switch (session.state) {
+            case PS_REQEST_LINE:
+            case PS_HEADERS:
+                parseLines(session, segment);
+                break;
+            case PS_BODY:
+                parseBody(session, segment);
+                break;
+            }
+        }
+
+        if (session.state == PS_END) {
+            if (session.responseCode === null) {
+                if (session.url === '/') {
+                    sendResponse(session);
+                } else {
+                    session.responseCode = 404;
+                    sendError(session, function(result) {
+                        if (result < 0) {
+                            log("send failed. cause=" + chrome.runtime.lastError.message);
+                            // TODO: Error handling
+                        }
+                        chrome.sockets.tcp.close(session.socketInfo.socketId);
+                        delete sessionTable[session.socketInfo.socketId];
+                    });
+                }
+            } else {
+                sendError(session, function(result) {
+                    if (result < 0) {
+                        log("send failed. cause=" + chrome.runtime.lastError.message);
+                        // TODO: Error handling
+                    }
+                    chrome.sockets.tcp.close(session.socketInfo.socketId);
+                    delete sessionTable[session.socketInfo.socketId];
+                });
+            }
+        }
+
+        function parseLines(session, segment) {
+            var i;
+
+            for (i = 0; i < segment.length && segment.state != PS_END; i++) {
+                switch (segment[i]) {
+                case CR:
+                    break;
+                case LF:
+                    if (session.linePos == 0) {
+                        if (session.state == PS_HEADERS) {
+                            if (session.method === "post") {
+                                if (session["content-length"] !== undefined) {
+                                    session.contentLength = parseInt(session["content-length"], 10);
+                                    session.bodyRead += segment.length - i;
+                                    if (session.contentLength <= session.bodyRead) {
+                                        session.state = PS_END;
+                                    } else {
+                                        session.state = PS_BODY;
+                                    }
+                                } else {
+                                    session.state = PS_END;
+                                    session.responseCode = 411;
+                                }
+                            } else {
+                                session.state = PS_END;
+                            }
+                        } else {
+                            session.state = PS_END;
+                            session.responseCode = 400;
+                        }
+                    } else {
+                        dispatchLine(session, new StringView(session.lineBuf, 'UTF-8', 0, session.linePos).toString());
+                        session.linePos = 0;
+                    }
+                    break;
+                default:
+                    if (session.linePos < HTTP_LINE_LIMIT) {
+                        session.lineBuf[session.linePos] = segment[i];
+                        session.linePos++;
+                    } else {
+                        session.state = PS_END;
+                        session.responseCode = 413;
+                    }
+                }
+            }
+        }
+
+        function dispatchLine(session, line) {
+            switch (session.state) {
+            case PS_REQEST_LINE:
+                parseRequestLine(session, line);
+                session.state = PS_HEADERS;
+                break;
+            case PS_HEADERS:
+                parseHeaderLine(session, line);
+                break;
+            }
+        }
+
+        function parseRequestLine(session, line) {
+            var parts = line.split(/\s+/);
+            if (parts.length === 3) {
+                session.method = parts[0].toLowerCase();
+                session.url = parts[1];
+                session.version = parts[2];
+            } else {
+                session.state = PS_END;
+                session.responseCode = 400;
+            }
+        }
+
+        function parseHeaderLine(session, line) {
+            var pos = line.indexOf(":");
+            var name, value;
+            if (pos === -1) {
+                session.state = PS_END;
+                session.responseCode = 400;
+            } else {
+                name = line.substring(0, pos);
+                value = line.substring(pos + 1);
+                session.headers[name.toLowerCase()] = value;
+            }
+        }
+
+        function parseBody(session, segment) {
+            session.bodyRead += segment.length;
+            if (session.contentLength <= session.bodyRead) {
+                session.state = PS_END;
+            }
+        }
+
+        function sendResponse(session) {
+            var response = "", strView;
+            response += "HTTP/1.0 200 OK\r\n";
+            response += "Content-Length: 13\r\n";
+            response += "\r\n";
+            response += "Hello, World!";
+            var strView = new StringView(response, "UTF-8");
+            chrome.sockets.tcp.send(session.socketInfo.socketId, strView.buffer, function(info) {
+                console.log("response sent! result=" + info.resultCode);
+                chrome.sockets.tcp.close(session.socketInfo.socketId);
+                delete sessionTable[session.socketInfo.socketId];
+            });
+        }
+
+        function sendError(session, callback) {
+            var response = "", strView;
+            response += "HTTP/1.0 " + session.responseCode + " " + RESPONSE_TABLE[session.responseCode] + "\r\n";
+            response += "Content-Length: 0\r\n";
+            response += "\r\n";
+            strView = new StringView(response, "UTF-8");
+            chrome.sockets.tcp.send(session.socketInfo.socketId, strView.buffer, function(info) {
+                if (callback) {
+                    callback(info.resultCode);
+                }
+            });
+        }
+    });
+
+    chrome.sockets.tcp.onReceiveError.addListener(function(info) {
+        log("onReceiveError. result=" + info.resultCode);
+
+        if (sessionTable[info.socketId]) {
+            chrome.sockets.tcp.close(info.socketId);
+            delete sessionTable[info.socketId];
+        }
+
+        // TODO: error handling
+    });
+
     function agent_start(callback) {
         chrome.sockets.udp.create({}, function(info) {
             chrome.sockets.udp.setMulticastLoopbackMode(info.socketId, true, function(result) {
@@ -107,28 +324,51 @@ var pisces = (function() {
                             return;
                         }
 
-                        socketId = info.socketId;
-                        if (callback) {
+                        udpSocketId = info.socketId;
+                        if (tcpSocketId && callback) {
                             callback();
                         }
                     });
                 });
             });
         });
+
+        chrome.sockets.tcpServer.create({}, function(info) {
+            chrome.sockets.tcpServer.listen(info.socketId, '0.0.0.0', agent.config.port, function(result) {
+                if (result < 0) {
+                    log("listening error. cause=" + chrome.rntime.lastError.message);
+                    return;
+                }
+
+                tcpSocketId = info.socketId;
+                if (udpSocketId && callback) {
+                    callback();
+                }
+            });
+        });
     }
 
     function agent_stop(callback) {
-        if (socketId) {
-            chrome.sockets.udp.leaveGroup(socketId, agent.config.groupAddress, function(result) {
+        if (udpSocketId) {
+            chrome.sockets.udp.leaveGroup(udpSocketId, agent.config.groupAddress, function(result) {
                 if (result < 0) {
                     log("leaving multicast group failed. cause=" + chrome.runtime.lastError.message);
                 }
-                chrome.sockets.udp.close(socketId, function() {
-                    socketId = null;
-                    if (callback) {
+                chrome.sockets.udp.close(udpSocketId, function() {
+                    udpSocketId = undefined;
+                    if (!tcpSocketId && callback) {
                         callback();
                     }
                 });
+            });
+        }
+
+        if (tcpSocketId) {
+            chrome.sockets.tcpServer.disconnect(tcpSocketId, function() {
+                tcpSocketId = undefined;
+                if (!udpSocketId && callback) {
+                    callback();
+                }
             });
         }
     }
@@ -158,7 +398,7 @@ var pisces = (function() {
         dataview.setSha1Hash(4 + 16 + 16, '0000000000000000000000000000000000000000');
         dataview.setString(4 + 16 + 16 + 20, 60, agent.config.username);
 
-        chrome.sockets.udp.send(socketId, buf, destAddr, agent.config.port, function(info) {
+        chrome.sockets.udp.send(udpSocketId, buf, destAddr, agent.config.port, function(info) {
             if (callback) {
                 callback(info.resultCode);
             }
@@ -208,7 +448,7 @@ var pisces = (function() {
         dataview.setUint8(0, PT_BYE, false);
         dataview.setUuid(4, agent.config.userId);
 
-        chrome.sockets.udp.send(socketId, buf, agent.config.groupAddress, agent.config.port, function(info) {
+        chrome.sockets.udp.send(udpSocketId, buf, agent.config.groupAddress, agent.config.port, function(info) {
             if (callback) {
                 callback(info.resultCode);
             }
@@ -274,7 +514,7 @@ var pisces = (function() {
         dataview.setUint32(4 + 16 + 4 + 8 + 20, info.message.length, false);
         dataview.setString(4 + 16 + 4 + 8 + 20 + 4, substr.length, substr);
 
-        chrome.sockets.udp.send(socketId, buf, agent.config.groupAddress, agent.config.port, function(info) {
+        chrome.sockets.udp.send(udpSocketId, buf, agent.config.groupAddress, agent.config.port, function(info) {
             if (info.resultCode < 0) {
                 log("send failed. cause=" + chrome.runtime.lastError.message);
             } else {
